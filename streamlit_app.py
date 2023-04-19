c394c14d06a6f8adea0aed46950bb1e40c4175dc
Sarah <86535632+salishrodinger@users.noreply.github.com>
2023-04-19 14:15:30 +0200

    deleted json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,11 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Writes the raw API response in the screen
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
+
 # Takes the JSON version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Show it
